421c8ab7c1c240aff6146738002c3cb3091fd50d
Jiri Pirko <jiri@mellanox.com>
2016-05-13 18:33:58 +0200

    mlxsw: router: Maintain nexthop MAC updated in HW for each neighbour
    In HW, the nexthop entries are in MAC. We create one entry for each
    neigh in KVD linear area and we keep that MAC updated.
    
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -638,6 +638,7 @@ struct mlxsw_sp_neigh_entry {
 	struct neighbour *n;
 	bool offloaded;
 	struct delayed_work dw;
+	int adj_index;
 };
 
 static const struct rhashtable_params mlxsw_sp_neigh_ht_params = {
@@ -709,6 +710,7 @@ int mlxsw_sp_router_neigh_construct(struct net_device *dev,
 	struct mlxsw_sp_neigh_entry *neigh_entry;
 	const unsigned char *addr = n->primary_key;
 	size_t addr_len = sizeof(__be32);
+	int adj_index;
 	int err;
 
 	if (n->tbl != &arp_tbl)
@@ -724,12 +726,25 @@ int mlxsw_sp_router_neigh_construct(struct net_device *dev,
 	neigh_entry = mlxsw_sp_neigh_entry_create(addr, addr_len, n->dev, n);
 	if (!neigh_entry)
 		return -ENOMEM;
+
+	/* Allocate a single KVD linear entry for each neigh entry, it will
+	 * serve for single entry nexthops.
+	 */
+	adj_index = mlxsw_sp_kvdl_alloc(mlxsw_sp, MLXSW_SP_KVDL_TYPE_SINGLE);
+	if (adj_index < 0) {
+		err = adj_index;
+		goto err_kvdl_alloc;
+	}
+	neigh_entry->adj_index = adj_index;
+
 	err = mlxsw_sp_neigh_entry_insert(mlxsw_sp, neigh_entry);
 	if (err)
 		goto err_neigh_entry_insert;
 	return 0;
 
 err_neigh_entry_insert:
+	mlxsw_sp_kvdl_free(mlxsw_sp, neigh_entry->adj_index);
+err_kvdl_alloc:
 	mlxsw_sp_neigh_entry_destroy(neigh_entry);
 	return err;
 }
@@ -750,6 +765,7 @@ void mlxsw_sp_router_neigh_destroy(struct net_device *dev,
 	if (!neigh_entry)
 		return;
 	mlxsw_sp_neigh_entry_remove(mlxsw_sp, neigh_entry);
+	mlxsw_sp_kvdl_free(mlxsw_sp, neigh_entry->adj_index);
 	mlxsw_sp_neigh_entry_destroy(neigh_entry);
 }
 
@@ -848,6 +864,7 @@ static void mlxsw_sp_router_neigh_update_hw(struct work_struct *work)
 	struct mlxsw_sp_port *mlxsw_sp_port = netdev_priv(n->dev);
 	struct mlxsw_sp *mlxsw_sp = mlxsw_sp_port->mlxsw_sp;
 	char rauht_pl[MLXSW_REG_RAUHT_LEN];
+	char ratr_pl[MLXSW_REG_RATR_LEN];
 	unsigned char ha[ETH_ALEN];
 	struct mlxsw_sp_rif *rif;
 	struct net_device *dev;
@@ -888,6 +905,13 @@ static void mlxsw_sp_router_neigh_update_hw(struct work_struct *work)
 		} else {
 			neigh_entry->offloaded = true;
 		}
+		mlxsw_reg_ratr_pack(ratr_pl, MLXSW_REG_RATR_OPCODE_WRITE_ENTRY,
+				    true, neigh_entry->adj_index, rif->rif);
+		mlxsw_reg_ratr_eth_entry_pack(ratr_pl, ha);
+		err = mlxsw_reg_write(mlxsw_sp->core, MLXSW_REG(ratr), ratr_pl);
+		if (err)
+			netdev_err(dev, "Failed to update nexthop MAC for %pI4h\n",
+				   &dip);
 	} else if (removing) {
 		mlxsw_reg_rauht_pack4(rauht_pl, RAUHT_WRITE_OP_DELETE,
 				      rif->rif, ha, dip);
