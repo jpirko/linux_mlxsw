0e135856ef0022498b663153418204eaf2f718be
Yotam Gigi <yotam.gi@gmail.com>
2016-09-28 13:46:52 +0300

    ife: Introduce new metadata tlv types
     - IFE_META_IFINDEX: Allow to pass ifindex value as part of the ife
       metadata
     - IFE_META_ORIG_SIZE: Allow to pass the original packet size as part of
       the ife metadata. Can be used in case that the packet is truncated
     - IFE_META_SIZE: Allow to pass the size of the encapsulated packet as
       part of the ife metadata
    
    Signed-off-by: Yotam Gigi <yotamg@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- include/uapi/linux/ife.h
+++ include/uapi/linux/ife.h
@@ -8,6 +8,9 @@
 #define IFE_META_PRIO 3
 #define IFE_META_QMAP 4
 #define IFE_META_TCINDEX 5
+#define IFE_META_IFINDEX 6
+#define IFE_META_ORIGSIZE 7
+#define IFE_META_SIZE 8
 
 /*Can be overridden at runtime by module option*/
 #define __IFE_META_MAX 6
