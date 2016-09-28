a08dbf3a34f996c2dc3902a639dd0d3046926b4d
Yotam Gigi <yotam.gi@gmail.com>
2016-09-28 13:46:53 +0300

    Introduce sample tc action
    This action allow the user to sample traffic matched by tc classifier.
    The sampling consists of choosing packets randomly, truncating them,
    adding some informative metadata regarding the interface and the original
    packet size and mark them with specific mark, to allow further tc rules to
    match and process. The marked sample packets are then injected into the
    device ingress qdisc using netif_receive_skb.
    
    The packets metadata is packed using the ife encapsulation protocol, and
    the outer packet's ethernet dest, source and eth_type, along with the
    rate, mark and the optional truncation size can be configured from
    userspace.
    
    Example:
    To sample ingress traffic from interface eth1, and redirect the sampled
    the sampled packets to interface dummy0, one may use the commands:
    
    tc qdisc add dev eth1 handle ffff: ingress
    
    tc filter add dev eth1 parent ffff: \
    	   matchall action sample rate 12 mark 17
    
    tc filter add parent ffff: dev eth1 protocol all \
    	   u32 match mark 172 0xff
    	   action mirred egress redirect dev dummy0
    
    Where the first command adds an ingress qdisc and the second starts
    sampling every 12'th packet on dev eth0 and marks the sampled packets with
    17. The command third catches the sampled packets, which are marked with
    17, and redirects them to dev dummy0.
    
    Signed-off-by: Yotam Gigi <yotamg@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

---
+++ include/net/tc_act/tc_sample.h
@@ -0,0 +1,88 @@
+#ifndef __NET_TC_SAMPLE_H
+#define __NET_TC_SAMPLE_H
+
+#include <net/act_api.h>
+#include <linux/tc_act/tc_sample.h>
+
+struct tcf_sample {
+	struct tc_action	common;
+	u32			rate;
+	u32			mark;
+	bool			truncate;
+	u32			trunc_size;
+	u32			packet_counter;
+	u8			eth_dst[ETH_ALEN];
+	u8			eth_src[ETH_ALEN];
+	u16			eth_type;
+	bool			eth_type_set;
+	struct list_head	tcfm_list;
+};
+#define to_sample(a) ((struct tcf_sample *)a)
+
+struct sample_packet_metadata {
+	int sample_size;
+	int orig_size;
+	int ifindex;
+};
+
+#if IS_ENABLED(CONFIG_NET_ACT_SAMPLE)
+struct ethhdr *sample_packet_pack(struct sk_buff *skb,
+				  struct sample_packet_metadata *metadata);
+#else
+static inline struct ethhdr *
+sample_packet_pack(struct sk_buff *skb, struct sample_packet_metadata *metadata)
+{
+	return NULL;
+}
+#endif
+
+static inline bool is_tcf_sample(const struct tc_action *a)
+{
+#ifdef CONFIG_NET_CLS_ACT
+	return a->ops && a->ops->type == TCA_ACT_SAMPLE;
+#else
+	return false;
+#endif
+}
+
+static inline __u32 tcf_sample_mark(const struct tc_action *a)
+{
+	return to_sample(a)->mark;
+}
+
+static inline __u32 tcf_sample_rate(const struct tc_action *a)
+{
+	return to_sample(a)->rate;
+}
+
+static inline bool tcf_sample_truncate(const struct tc_action *a)
+{
+	return to_sample(a)->truncate;
+}
+
+static inline int tcf_sample_trunc_size(const struct tc_action *a)
+{
+	return to_sample(a)->trunc_size;
+}
+
+static inline u16 tcf_sample_eth_type(const struct tc_action *a)
+{
+	return to_sample(a)->eth_type;
+}
+
+static inline bool tcf_sample_eth_type_set(const struct tc_action *a)
+{
+	return to_sample(a)->eth_type_set;
+}
+
+static inline void tcf_sample_eth_dst_addr(const struct tc_action *a, u8 *dst)
+{
+	ether_addr_copy(dst, to_sample(a)->eth_dst);
+}
+
+static inline void tcf_sample_eth_src_addr(const struct tc_action *a, u8 *src)
+{
+	ether_addr_copy(src, to_sample(a)->eth_src);
+}
+
+#endif /* __NET_TC_SAMPLE_H */
