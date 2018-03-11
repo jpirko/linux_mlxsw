be76b912b2bfde227f8d2ec045a02a68957846af
Yuval Mintz <yuvalm@mellanox.com>
2018-03-11 12:37:35 +0200

    selftests: forwarding: Allow lib.sh sourcing from other directories
    The devlink related scripts are mlxsw-specific. As a result, they'll
    reside in a different directory - but would still need the common logic
    implemented in lib.sh.
    So as a preliminary step, allow lib.sh to be sourced from other
    directories as well.
    
    Signed-off-by: Yuval Mintz <yuvalm@mellanox.com>
    Signed-off-by: Ido Schimmel <idosch@mellanox.com>

--- tools/testing/selftests/net/forwarding/lib.sh
+++ tools/testing/selftests/net/forwarding/lib.sh
@@ -12,8 +12,13 @@ WAIT_TIME=${WAIT_TIME:=5}
 PAUSE_ON_FAIL=${PAUSE_ON_FAIL:=no}
 PAUSE_ON_CLEANUP=${PAUSE_ON_CLEANUP:=no}
 
-if [[ -f forwarding.config ]]; then
-	source forwarding.config
+relative_path="${BASH_SOURCE%/*}"
+if [[ "$relative_path" == "${BASH_SOURCE}" ]]; then
+	relative_path="."
+fi
+
+if [[ -f $relative_path/forwarding.config ]]; then
+	source "$relative_path/forwarding.config"
 fi
 
 ##############################################################################
