01e3d408d82d9b1ed7c55dbee4719db0d42d1a44
Alex Vesker <valex@mellanox.com>
2018-03-21 13:55:12 +0200

    devlink: Add callback to query for snapshot id before snapshot create
    To restrict the driver with the snapshot ID selection a new callback
    is introduced for the driver to get the snapshot ID before creating
    a new snapshot. This will also allow giving the same ID for multiple
    snapshots taken of different regions on the same time.
    
    Signed-off-by: Alex Vesker <valex@mellanox.com>
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -29,6 +29,7 @@ struct devlink {
 	struct list_head resource_list;
 	struct devlink_dpipe_headers *dpipe_headers;
 	struct list_head region_list;
+	u32 snapshot_id;
 	const struct devlink_ops *ops;
 	struct device *dev;
 	possible_net_t _net;
@@ -428,6 +429,7 @@ struct devlink_region *devlink_region_create(struct devlink *devlink,
 					     u32 region_max_snapshots,
 					     u64 region_size);
 void devlink_region_destroy(struct devlink_region *region);
+u32 devlink_region_shapshot_id_get(struct devlink *devlink);
 
 #else
 
@@ -611,6 +613,12 @@ devlink_region_create(struct devlink *devlink,
 static inline void
 devlink_region_destroy(struct devlink_region *region)
 {
+}
+
+static inline u32
+devlink_region_shapshot_id_get(struct devlink *devlink)
+{
+	return 0;
 }
 
 #endif
