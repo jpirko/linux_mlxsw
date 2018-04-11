02063138a4d305ecb6a86ab1bbe659b55db3e5b3
Jiri Pirko <jiri@mellanox.com>
2018-04-11 15:18:19 +0200

    selftests: tc_common: Add polling for results and timeout
    Since some drivers, like for example mlx5 are getting stats from HW
    periodically, the tests are currently failing. Add poll-timeout
    mechanism for getting the TC stats.
    
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>

--- tools/testing/selftests/net/forwarding/tc_common.sh
+++ tools/testing/selftests/net/forwarding/tc_common.sh
@@ -10,16 +10,29 @@ tc_check_packets()
 	local count=$3
 	local ret
 
-	output="$(tc -j -s filter show $id)"
-	# workaround the jq bug which causes jq to return 0 in case input is ""
-	ret=$?
-	if [[ $ret -ne 0 ]]; then
-		return $ret
-	fi
-	echo $output | \
-		jq -e ".[] \
-		| select(.options.handle == $handle) \
-		| select(.options.actions[0].stats.packets == $count)" \
-		&> /dev/null
-	return $?
+	local timeout=2000
+	local starttime="$(date +%s%3N)"
+	local endtime=$((starttime + timeout))
+
+	local currenttime=$starttime
+	while (( $currenttime <= $endtime )); do
+		output="$(tc -j -p -s filter show $id)"
+		# workaround the jq bug which causes jq to return 0 in case input is ""
+		ret=$?
+		if [[ $ret -ne 0 ]]; then
+			return $ret
+		fi
+		echo $output | \
+			jq -e ".[] \
+			| select(.options.handle == $handle) \
+			| select(.options.actions[0].stats.packets == $count)" \
+			&> /dev/null
+		ret=$?
+		if [[ $ret -eq 0 ]]; then
+			return 0
+		fi
+		currenttime="$(date +%s%3N)"
+	done
+
+	return 1
 }
