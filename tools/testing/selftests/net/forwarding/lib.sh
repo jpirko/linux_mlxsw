7226eccd76c07333405d1931f1fade79c92587f0
Petr Machata <petrm@mellanox.com>
2020-01-27 18:54:33 +0200

    selftests: forwarding: lib: Have humanize() support custom suffixes
    The function humanize() is used when converting a large accurate value in
    bps to one that uses SI prefixes to make the value better suited for human
    consumption. The same algorithm will work regardless of the actual unit, so
    make the function more generic by providing the unit as an argument.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>

--- tools/testing/selftests/net/forwarding/lib.sh
+++ tools/testing/selftests/net/forwarding/lib.sh
@@ -850,8 +850,9 @@ hw_stats_get()
 humanize()
 {
 	local speed=$1; shift
+	local suffix=${1:-bps}; shift
 
-	for unit in bps Kbps Mbps Gbps; do
+	for unit in "$suffix" "K$suffix" "M$suffix" "G$suffix"; do
 		if (($(echo "$speed < 1024" | bc))); then
 			break
 		fi
