e8e50cac40b7a21a79961d5e1b3a06803834e833
Petr Machata <petrm@mellanox.com>
2020-05-21 11:07:42 +0300

    net: sched: Introduce helpers for qevent blocks
    Qevents are attach points for TC blocks, where filters can be put that are
    executed when "interesting events" take place in a qdisc. The data to keep
    and the functions to invoke to maintain a qevent will be largely the same
    between qevents. Therefore introduce a sched-wide helpers for qevent
    management.
    
    Currently, similarly to ingress and egress blocks of clsact pseudo-qdisc,
    blocks attachment cannot be changed after the qdisc is created. To that
    end, add a helper tcf_qevent_validate_change(), which verifies whether
    block index attribute is not attached, or if it is, whether its value
    matches the current one (i.e. there is no material change).
    
    The function tcf_qevent_handle() is supposed to be invoked when qdisc hits
    the "interesting event" corresponding to this qeven block.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>

--- include/net/pkt_cls.h
+++ include/net/pkt_cls.h
@@ -32,6 +32,13 @@ struct tcf_block_ext_info {
 	u32 block_index;
 };
 
+struct tcf_qevent {
+	int			attr_name;
+	struct tcf_block	*block;
+	struct tcf_block_ext_info info;
+	struct tcf_proto __rcu *filter_chain;
+};
+
 struct tcf_block_cb;
 bool tcf_queue_work(struct rcu_work *rwork, work_func_t func);
 
@@ -551,6 +558,47 @@ int tc_setup_cb_reoffload(struct tcf_block *block, struct tcf_proto *tp,
 			  enum tc_setup_type type, void *type_data,
 			  void *cb_priv, u32 *flags, unsigned int *in_hw_count);
 unsigned int tcf_exts_num_actions(struct tcf_exts *exts);
+
+#ifdef CONFIG_NET_CLS_ACT
+int tcf_qevent_init(struct tcf_qevent *qe, struct Qdisc *sch,
+		    enum flow_block_binder_type binder_type,
+		    struct nlattr *block_index_attr,
+		    struct netlink_ext_ack *extack);
+void tcf_qevent_destroy(struct tcf_qevent *qe, struct Qdisc *sch);
+int tcf_qevent_validate_change(struct tcf_qevent *qe,
+			       struct nlattr *block_index_attr,
+			       struct netlink_ext_ack *extack);
+struct sk_buff *tcf_qevent_handle(struct tcf_qevent *qe, struct Qdisc *sch,
+				  struct sk_buff *skb, struct sk_buff **to_free,
+				  int *ret);
+#else
+static inline int tcf_qevent_init(struct tcf_qevent *qe, struct Qdisc *sch,
+				  enum flow_block_binder_type binder_type,
+				  struct nlattr *block_index_attr,
+				  struct netlink_ext_ack *extack)
+{
+	return 0;
+}
+
+static inline void tcf_qevent_destroy(struct tcf_qevent *qe, struct Qdisc *sch)
+{
+}
+
+static inline int tcf_qevent_validate_change(struct tcf_qevent *qe,
+					     struct nlattr *block_index_attr,
+					     struct netlink_ext_ack *extack)
+{
+	return 0;
+}
+
+static inline struct sk_buff *
+tcf_qevent_handle(struct tcf_qevent *qe, struct Qdisc *sch,
+		  struct sk_buff *skb, struct sk_buff **to_free,
+		  int *ret)
+{
+	return skb;
+}
+#endif
 
 struct tc_cls_u32_knode {
 	struct tcf_exts *exts;
