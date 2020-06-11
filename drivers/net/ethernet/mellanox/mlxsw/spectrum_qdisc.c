706ff497018d6c407657c31ac9c4d79d212bc7bb
Petr Machata <petrm@mellanox.com>
2020-06-12 01:00:42 +0300

    mlxsw: spectrum_qdisc: Make RED, TBF offloads classful
    Permit offloading qdiscs below RED and TBF. In order to avoid having to
    implement trivial propagating callbacks for get_prio_bitmap and get_tclass_num,
    extend mlxsw_sp_qdisc_get_prio_bitmap() and ..._get_tclass_num() to handle the
    lack of the callback as a cue to forward the request to the parent.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_qdisc.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_qdisc.c
@@ -197,6 +197,8 @@ static u8 mlxsw_sp_qdisc_get_prio_bitmap(struct mlxsw_sp_port *mlxsw_sp_port,
 {
 	if (!mlxsw_sp_qdisc->parent)
 		return 0xff;
+	if (!mlxsw_sp_qdisc->parent->ops->get_prio_bitmap)
+		return mlxsw_sp_qdisc_get_prio_bitmap(mlxsw_sp_port, mlxsw_sp_qdisc->parent);
 	return mlxsw_sp_qdisc->parent->ops->get_prio_bitmap(mlxsw_sp_qdisc->parent, mlxsw_sp_qdisc);
 }
 
@@ -207,6 +209,8 @@ static int mlxsw_sp_qdisc_get_tclass_num(struct mlxsw_sp_port *mlxsw_sp_port,
 {
 	if (!mlxsw_sp_qdisc->parent)
 		return MLXSW_SP_PORT_DEFAULT_TCLASS;
+	if (!mlxsw_sp_qdisc->parent->ops->get_tclass_num)
+		return mlxsw_sp_qdisc_get_tclass_num(mlxsw_sp_port, mlxsw_sp_qdisc->parent);
 	return mlxsw_sp_qdisc->parent->ops->get_tclass_num(mlxsw_sp_qdisc->parent, mlxsw_sp_qdisc);
 }
 
@@ -626,6 +630,11 @@ mlxsw_sp_qdisc_red_check_params(struct mlxsw_sp_port *mlxsw_sp_port,
 	return 0;
 }
 
+static int mlxsw_sp_qdisc_future_fifo_replace(struct mlxsw_sp_port *mlxsw_sp_port,
+					      u32 handle, unsigned int band,
+					      struct mlxsw_sp_qdisc *child_qdisc);
+static void mlxsw_sp_qdisc_future_fifos_init(struct mlxsw_sp_port *mlxsw_sp_port, u32 handle);
+
 static int
 mlxsw_sp_qdisc_red_replace(struct mlxsw_sp_port *mlxsw_sp_port, u32 handle,
 			   struct mlxsw_sp_qdisc *mlxsw_sp_qdisc,
@@ -636,6 +645,13 @@ mlxsw_sp_qdisc_red_replace(struct mlxsw_sp_port *mlxsw_sp_port, u32 handle,
 	struct tc_red_qopt_offload_params *p = params;
 	u32 min, max;
 	u64 prob;
+	int err;
+
+	err = mlxsw_sp_qdisc_future_fifo_replace(mlxsw_sp_port, handle, 0,
+						 &mlxsw_sp_qdisc->qdiscs[0]);
+	if (err)
+		return err;
+	mlxsw_sp_qdisc_future_fifos_init(mlxsw_sp_port, TC_H_UNSPEC);
 
 	/* calculate probability in percentage */
 	prob = p->probability;
@@ -722,7 +738,10 @@ mlxsw_sp_qdisc_get_red_stats(struct mlxsw_sp_port *mlxsw_sp_port,
 static struct mlxsw_sp_qdisc *
 mlxsw_sp_qdisc_leaf_find_class(struct mlxsw_sp_qdisc *mlxsw_sp_qdisc, u32 parent)
 {
-	return NULL;
+	/* RED and TBF are formally classful qdiscs, but all class references,
+	 * including X:0, just refer to the same one class.
+	 */
+	return &mlxsw_sp_qdisc->qdiscs[0];
 }
 
 static struct mlxsw_sp_qdisc_ops mlxsw_sp_qdisc_ops_red = {
@@ -735,6 +754,7 @@ static struct mlxsw_sp_qdisc_ops mlxsw_sp_qdisc_ops_red = {
 	.get_xstats = mlxsw_sp_qdisc_get_red_xstats,
 	.clean_stats = mlxsw_sp_setup_tc_qdisc_red_clean_stats,
 	.find_class = mlxsw_sp_qdisc_leaf_find_class,
+	.num_classes = 1,
 };
 
 static int mlxsw_sp_qdisc_graft(struct mlxsw_sp_port *mlxsw_sp_port,
@@ -901,6 +921,12 @@ mlxsw_sp_qdisc_tbf_replace(struct mlxsw_sp_port *mlxsw_sp_port, u32 handle,
 	u8 burst_size;
 	int err;
 
+	err = mlxsw_sp_qdisc_future_fifo_replace(mlxsw_sp_port, handle, 0,
+						 &mlxsw_sp_qdisc->qdiscs[0]);
+	if (err)
+		return err;
+	mlxsw_sp_qdisc_future_fifos_init(mlxsw_sp_port, TC_H_UNSPEC);
+
 	err = mlxsw_sp_qdisc_tbf_bs(mlxsw_sp_port, p->max_size, &burst_size);
 	if (WARN_ON_ONCE(err))
 		/* check_params above was supposed to reject this value. */
@@ -949,6 +975,7 @@ static struct mlxsw_sp_qdisc_ops mlxsw_sp_qdisc_ops_tbf = {
 	.get_stats = mlxsw_sp_qdisc_get_tbf_stats,
 	.clean_stats = mlxsw_sp_setup_tc_qdisc_leaf_clean_stats,
 	.find_class = mlxsw_sp_qdisc_leaf_find_class,
+	.num_classes = 1,
 };
 
 static int __mlxsw_sp_setup_tc_tbf(struct mlxsw_sp_port *mlxsw_sp_port,
