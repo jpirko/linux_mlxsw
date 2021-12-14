6c7d83f948d1ed2f3ee3743cd8e9c56043afcb27
Vadim Pasternak <vadimp@nvidia.com>
2021-12-14 10:57:34 +0200

    mlxsw: core_thermal: Split gearbox initialization
    Split gearbox initialization in two routines - the first one is to be
    used for gearbox configuration validation, the second for creation of
    gearbox related thermal zones if any.
    
    Currently, mlxsw supports gearbox thermal zones corresponding to the
    main board. For system equipped with the line cards assembled with the
    gearboxes, thermal zones will be associated with the gearboxes found on
    those line cards.
    
    While the initialization flow for main board and for line cards is the
    same, the configuration flow is different.
    
    The purpose of this patch is to allow reusing of initialization flow by
    main board and line cards.
    
    Signed-off-by: Vadim Pasternak <vadimp@nvidia.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Ido Schimmel <idosch@nvidia.com>

--- drivers/net/ethernet/mellanox/mlxsw/core_thermal.c
+++ drivers/net/ethernet/mellanox/mlxsw/core_thermal.c
@@ -846,15 +846,12 @@ mlxsw_thermal_gearbox_tz_fini(struct mlxsw_thermal_module *gearbox_tz)
 }
 
 static int
-mlxsw_thermal_gearboxes_init(struct device *dev, struct mlxsw_core *core,
-			     struct mlxsw_thermal *thermal,
-			     struct mlxsw_thermal_area *area)
+mlxsw_thermal_gearboxes_main_init(struct device *dev, struct mlxsw_core *core,
+				  struct mlxsw_thermal_area *area)
 {
 	enum mlxsw_reg_mgpir_device_type device_type;
-	struct mlxsw_thermal_module *gearbox_tz;
 	char mgpir_pl[MLXSW_REG_MGPIR_LEN];
 	u8 gbox_num;
-	int i;
 	int err;
 
 	mlxsw_reg_mgpir_pack(mgpir_pl, 0);
@@ -864,8 +861,11 @@ mlxsw_thermal_gearboxes_init(struct device *dev, struct mlxsw_core *core,
 
 	mlxsw_reg_mgpir_unpack(mgpir_pl, &gbox_num, &device_type, NULL,
 			       NULL, NULL);
-	if (device_type != MLXSW_REG_MGPIR_DEVICE_TYPE_GEARBOX_DIE ||
-	    !gbox_num)
+	if (device_type != MLXSW_REG_MGPIR_DEVICE_TYPE_GEARBOX_DIE)
+		gbox_num = 0;
+
+	/* Skip gearbox sensor array allocation, if no gearboxes are available. */
+	if (!gbox_num)
 		return 0;
 
 	area->tz_gearbox_num = gbox_num;
@@ -874,6 +874,26 @@ mlxsw_thermal_gearboxes_init(struct device *dev, struct mlxsw_core *core,
 				       GFP_KERNEL);
 	if (!area->tz_gearbox_arr)
 		return -ENOMEM;
+
+	return 0;
+}
+
+static void
+mlxsw_thermal_gearboxes_main_fini(struct mlxsw_thermal_area *area)
+{
+	kfree(area->tz_gearbox_arr);
+}
+
+static int
+mlxsw_thermal_gearboxes_init(struct device *dev, struct mlxsw_core *core,
+			     struct mlxsw_thermal *thermal,
+			     struct mlxsw_thermal_area *area)
+{
+	struct mlxsw_thermal_module *gearbox_tz;
+	int i, err;
+
+	if (!area->tz_gearbox_num)
+		return 0;
 
 	for (i = 0; i < area->tz_gearbox_num; i++) {
 		gearbox_tz = &area->tz_gearbox_arr[i];
@@ -892,7 +912,6 @@ mlxsw_thermal_gearboxes_init(struct device *dev, struct mlxsw_core *core,
 err_thermal_gearbox_tz_init:
 	for (i--; i >= 0; i--)
 		mlxsw_thermal_gearbox_tz_fini(&area->tz_gearbox_arr[i]);
-	kfree(area->tz_gearbox_arr);
 	return err;
 }
 
@@ -904,7 +923,6 @@ mlxsw_thermal_gearboxes_fini(struct mlxsw_thermal *thermal,
 
 	for (i = area->tz_gearbox_num - 1; i >= 0; i--)
 		mlxsw_thermal_gearbox_tz_fini(&area->tz_gearbox_arr[i]);
-	kfree(area->tz_gearbox_arr);
 }
 
 int mlxsw_thermal_init(struct mlxsw_core *core,
@@ -1003,6 +1021,10 @@ int mlxsw_thermal_init(struct mlxsw_core *core,
 	if (err)
 		goto err_thermal_modules_init;
 
+	err = mlxsw_thermal_gearboxes_main_init(dev, core, thermal->main);
+	if (err)
+		goto err_thermal_gearboxes_main_init;
+
 	err = mlxsw_thermal_gearboxes_init(dev, core, thermal, thermal->main);
 	if (err)
 		goto err_thermal_gearboxes_init;
@@ -1017,6 +1039,8 @@ int mlxsw_thermal_init(struct mlxsw_core *core,
 err_thermal_zone_device_enable:
 	mlxsw_thermal_gearboxes_fini(thermal, thermal->main);
 err_thermal_gearboxes_init:
+	mlxsw_thermal_gearboxes_main_fini(thermal->main);
+err_thermal_gearboxes_main_init:
 	mlxsw_thermal_modules_fini(thermal, thermal->main);
 err_thermal_modules_init:
 	if (thermal->tzdev) {
@@ -1041,6 +1065,7 @@ void mlxsw_thermal_fini(struct mlxsw_thermal *thermal)
 	int i;
 
 	mlxsw_thermal_gearboxes_fini(thermal, thermal->main);
+	mlxsw_thermal_gearboxes_main_fini(thermal->main);
 	mlxsw_thermal_modules_fini(thermal, thermal->main);
 	if (thermal->tzdev) {
 		thermal_zone_device_unregister(thermal->tzdev);
