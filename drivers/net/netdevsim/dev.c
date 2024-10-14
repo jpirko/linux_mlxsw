fd24adbf5b25075082d48fcdfba57a434864a24c
Jiri Pirko <jiri@nvidia.com>
2024-10-14 14:26:02 +0200

    devlink: make devl_register() return void
    The function always return 0. Convert it to remove void, also adjust the
    only caller which is checking the return value, netdevsim.
    
    Signed-off-by: Jiri Pirko <jiri@nvidia.com>

--- drivers/net/netdevsim/dev.c
+++ drivers/net/netdevsim/dev.c
@@ -1556,9 +1556,7 @@ int nsim_drv_probe(struct nsim_bus_dev *nsim_bus_dev)
 		goto err_devlink_unlock;
 	}
 
-	err = devl_register(devlink);
-	if (err)
-		goto err_vfc_free;
+	devl_register(devlink);
 
 	err = nsim_dev_resources_register(devlink);
 	if (err)
@@ -1635,7 +1633,6 @@ err_resource_unregister:
 	devl_resources_unregister(devlink);
 err_dl_unregister:
 	devl_unregister(devlink);
-err_vfc_free:
 	kfree(nsim_dev->vfconfigs);
 err_devlink_unlock:
 	devl_unlock(devlink);
